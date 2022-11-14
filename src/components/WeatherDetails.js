2b9449d44bd75e76e7fca8f485893d4a0a8c833f
poojaganjage <pooja260895@gmail.com>
2022-11-14 17:42:12 +0530

    WeatherDetails File Updated

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -35,60 +35,60 @@ function WeatherDetails({temp, pressure, humidity, weatherType, name, speed, cou
   return (
     <>
         <article className='widget'>
-            <div className="weatherIcon">
+            <div className='weatherIcon'>
                 {/* <i className='wi wi-day-sunny'></i> */}
                 <i className={`wi ${weatherState}`}></i>
             </div>
-            <div className="weatherInfo">
-                <div className="temperature">
+            <div className='weatherInfo'>
+                <div className='temperature'>
                     <span>{temp}&deg;</span>
                 </div>
-                <div className="description">
-                    <div className="weatherCondition">
+                <div className='description'>
+                    <div className='weatherCondition'>
                         {weatherType}
                     </div>
-                    <div className="place">
+                    <div className='place'>
                         {name}, {country}
                     </div>
                 </div>
             </div>
-            <div className="date">{new Date().toLocaleString()}</div>
-            <div className="extra-temp">
-                <div className="temp-info-minmax">
-                    <div className="two-sided-section">
+            <div className='date'>{new Date().toLocaleString()}</div>
+            <div className='extra-temp'>
+                <div className='temp-info-minmax'>
+                    <div className='two-sided-section'>
                         <p>
                             <i className={'wi wi-sunset'}></i>
                         </p>
-                        <p className="extra-info-leftside">
+                        <p className='extra-info-leftside'>
                             {timeStr} PM <br />
                             Sunset
                         </p>
                     </div>
-                    <div className="two-sided-section">
+                    <div className='two-sided-section'>
                         <p>
                             <i className={'wi wi-humidity'}></i>
                         </p>
-                        <p className="extra-info-leftside">
+                        <p className='extra-info-leftside'>
                             {humidity} <br />
                             Humidity
                         </p>
                     </div>
                 </div>
-                <div className="weather-extra-info">
-                    <div className="two-sided-section">
+                <div className='weather-extra-info'>
+                    <div className='two-sided-section'>
                         <p>
                             <i className={'wi wi-rain'}></i>
                         </p>
-                        <p className="extra-info-leftside">
+                        <p className='extra-info-leftside'>
                             {pressure} <br />
                             Pressure
                         </p>
                     </div>
-                    <div className="two-sided-section">
+                    <div className='two-sided-section'>
                         <p>
                             <i className={'wi wi-strong-wind'}></i>
                         </p>
-                        <p className="extra-info-leftside">
+                        <p className='extra-info-leftside'>
                             {speed} <br />
                             Speed
                         </p>
